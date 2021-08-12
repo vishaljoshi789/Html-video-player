b8656da4bea94f9f3ddf1afc287a811805c70687
Vishal Joshi <72333980+vishaljoshi789@users.noreply.github.com>
2021-08-12 19:32:29 +0530

    Add files via upload

--- js/controls.js
+++ js/controls.js
@@ -6,6 +6,9 @@ let currentTimeElement = document.querySelector('.current')
 let durationTimeElement = document.querySelector('.duration')
 let left_skip = document.querySelector('.left_skip')
 let right_skip = document.querySelector('.right_skip')
+let skip_ad = document.querySelector('.skip_ad')
+let resolution_selector = document.querySelector('.resolution_selector')
+
 
 // play pause function 
 // play_pause_btn.addEventListener('click',()=>{
@@ -50,24 +53,101 @@ let right_skip = document.querySelector('.right_skip')
 
 // currentTime and duration
 
-let currentTime = ()=> {
-    let currentMinutes = Math.floor(video_player.currentTime / 60);
-    let currentSeconds = Math.floor(video_player.currentTime - currentMinutes * 60)
-    let durationMinutes = Math.floor(video_player.duration / 60);
-    let durationSeconds = Math.floor(video_player.duration - durationMinutes * 60)
+// let currentTime = ()=> {
+//     let currentMinutes = Math.floor(video_player.currentTime / 60);
+//     let currentSeconds = Math.floor(video_player.currentTime - currentMinutes * 60)
+//     let durationMinutes = Math.floor(video_player.duration / 60);
+//     let durationSeconds = Math.floor(video_player.duration - durationMinutes * 60)
 
-    currentTimeElement.innerHTML = `${currentMinutes < 10 ? '0'+ currentMinutes : currentMinutes}:${currentSeconds < 10 ? '0'+ currentSeconds : currentSeconds}`
-    durationTimeElement.innerHTML = `${durationMinutes < 10 ? '0'+ durationMinutes : durationMinutes}:${durationSeconds < 10 ? '0'+ durationSeconds : durationSeconds}`
-}
+//     currentTimeElement.innerHTML = `${currentMinutes < 10 ? '0'+ currentMinutes : currentMinutes}:${currentSeconds < 10 ? '0'+ currentSeconds : currentSeconds}`
+//     durationTimeElement.innerHTML = `${durationMinutes < 10 ? '0'+ durationMinutes : durationMinutes}:${durationSeconds < 10 ? '0'+ durationSeconds : durationSeconds}`
+// }
 
-video_player.addEventListener('timeupdate', currentTime)
+// video_player.addEventListener('timeupdate', currentTime)
 
 
 // left right skip 
 
-left_skip.addEventListener('click',()=>{
+left_skip.addEventListener('dblclick',()=>{
     video_player.currentTime = video_player.currentTime - 10
 })
-right_skip.addEventListener('click',()=>{
+right_skip.addEventListener('dblclick',()=>{
     video_player.currentTime = video_player.currentTime + 10
 })
+
+
+// ad skipping
+
+i = 5
+ad_time = setInterval(() => {
+            if(i==0){
+                skip_ad.innerHTML = `Skip ad`
+                clearInterval(ad_time)
+            }else{
+            skip_ad.innerHTML = `${i}`}
+            i-=1
+        }, 1000);
+
+
+skip_ad.addEventListener('click',()=>{
+    if(skip_ad.innerHTML == `Skip ad`){
+            video_player.src = 'videos/sample-video2.mp4'
+            video_player.controls = 'True'
+            skip_ad.innerHTML = ``
+            resolution_selector.style.opacity = '1'
+            setTimeout(() => {
+                resolution_selector.style.opacity = 0
+            },2000);
+            video_player.addEventListener('mouseenter', ()=>{
+                resolution_selector.style.opacity = 1
+                setTimeout(() => {
+                    resolution_selector.style.opacity = 0
+                },2000);
+        })
+    }
+})
+
+video_player.addEventListener('timeupdate',()=>{
+    if(video_player.currentTime == video_player.duration){
+            video_player.src = 'videos/sample-video2.mp4'
+            video_player.controls = 'True'
+            skip_ad.innerHTML = ``
+            resolution_selector.style.opacity = '1'
+            setTimeout(() => {
+                resolution_selector.style.opacity = 0
+            },2000);
+            video_player.addEventListener('mouseenter', ()=>{
+                resolution_selector.style.opacity = 1
+                setTimeout(() => {
+                    resolution_selector.style.opacity = 0
+                },2000);
+        })
+    }
+})
+
+
+// resolution selector 
+
+resolution_selector.addEventListener('change', ()=>{
+    resolution = document.getElementById('resolution').value
+    if(resolution == '360'){
+        previousTime = video_player.currentTime
+        video_player.src = 'videos/sample-video2_360.mp4'
+        video_player.currentTime = previousTime
+    }else if(resolution == '720'){
+        previousTime = video_player.currentTime
+        video_player.src = 'videos/sample-video2.mp4'
+        video_player.currentTime = previousTime
+    }else if(resolution == '480'){
+        previousTime = video_player.currentTime
+        video_player.src = 'videos/sample-video2_480.mp4'
+        video_player.currentTime = previousTime
+    }
+})
+
+
+// autohiding
+
+
+
+
